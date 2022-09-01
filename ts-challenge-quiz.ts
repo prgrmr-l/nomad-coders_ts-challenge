cb4bc6d04a3b3db7d846171bd16e2dbbc44aa288
yougotmyphone <110523886+yougotmyphone@users.noreply.github.com>
2022-09-01 21:10:52 +0900

    3-answer&&quiz

---
+++ ts-challenge-quiz.ts
@@ -0,0 +1,55 @@
+// <!--
+
+// QUESTION 1 OF 8
+
+// INCORRECT
+// Inside of tsconfig.json what does the include property do?
+// It tells TS where to put the output code
+// YOUR ANSWER
+// It tells TS where to look for code to compile
+// CORRECT ANSWER
+
+// QUESTION 2 OF 8
+
+// INCORRECT
+// Inside of tsconfig.json what does the outDir property do?
+// It tells TS where to look for code to compile
+// YOUR ANSWER
+// It tells TS where to put the output code
+// CORRECT ANSWER
+
+// QUESTION 3 OF 8
+
+// CORRECT
+// Inside of tsconfig.json what does the target property do?
+// It specifies the version of JS we want to compile to the to
+
+// QUESTION 4 OF 8
+
+// CORRECT
+// Inside of tsconfig.json what does the lib property do?
+// It specifies what environment the code is goin to run on
+
+// QUESTION 5 OF 8
+
+// CORRECT
+// What is a Type Definition file?
+// A TS file with comments that explains to TS the types of JS code
+
+// QUESTION 6 OF 8
+
+// CORRECT
+// Can we use Typescript and Javascript in the same project?
+// Yes
+
+// QUESTION 7 OF 8
+
+// CORRECT
+// What does //@ts-check do?
+// It tells TS to type check JS files
+
+// QUESTION 8 OF 8
+
+// CORRECT
+// Can we use JSDoc on any JS file?
+// Yes -->
